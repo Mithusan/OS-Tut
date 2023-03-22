2a96f7c8f34ee7d09d7aa4ff0919118d45541398
Mithusan <mithusan.arul11@gmail.com>
2023-03-21 23:25:17 -0400

    CertifiedAwaisLoverBoy

---
+++ Tutorial 7/Code/task4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+
+int main(void)
+{
+	pid_t parent = getpid();
+	pid_t pid = fork();
+	
+		if(pid == -1){
+		printf("fork failed");
+		}
+		
+		else if(pid > 0){
+		int status;
+		waitpid(pid, &status, 0);
+		}
+		
+		else{
+		pid_t pid_process = getpid();
+		int status;
+		char * argv_list[] = {"process",NULL};
+		execv("process", argv_list);
+		wait(5);
+		kill(pid_process,SIGTSTP);
+		wait(10);
+		kill(pid_process,SIGCONT);
+		exit(0);
+		}
+	
+}
+	
+	
+
