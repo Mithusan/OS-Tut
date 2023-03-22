7da6e7be358f5a1d972ce81b406cbbdba8466564
Mithusan <mithusan.arul11@gmail.com>
2023-03-21 21:45:42 -0400

    e

--- Tutorial 7/Code/task1.c
+++ Tutorial 7/Code/task1.c
@@ -1,25 +1,43 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 
-typedef struct process {
+struct proc {
     char name[256];
     int priority;
     int pid;
     int runtime;
-} proc;
+};
 
-typedef struct linked_list {
-    proc process;
-    queue linked_list;
-} queue;
+struct queue {
+    struct proc process;
+    struct queue *linked_list;
+};
 
-void push(proc process){
-    
+struct queue head;
+
+//used to find a node in a chain of linked lists
+struct queue traverse(struct queue head, int index){
+    struct queue currIndex = head;
+    int i = 0;
+    do {
+        if (i < index){
+            currIndex = *currIndex.linked_list;
+        }
+        else if (i >= index){
+            return currIndex;
+        }
+        i++;
+    } while(currIndex.linked_list != NULL);
+
+    return currIndex;
 }
+
+void push(struct proc process) {
+    struct queue newNode;
+    newNode.process = process;
+    *traverse(head, 200).linked_list = newNode;
+} 
 
 int main(void){
     
