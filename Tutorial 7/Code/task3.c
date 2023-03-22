7ac9499c27bd7d31e8c7434c6e1f90c3efb178e0
Mithusan <mithusan.arul11@gmail.com>
2023-03-22 00:11:21 -0400

    Co-authored-by: awaisahmad-otu <awaisahmad-otu@users.noreply.github.com> Co-authored-by: Mohammad Mohammadkhani <CaptainMetal080@users.noreply.github.com>

--- Tutorial 7/Code/task3.c
+++ Tutorial 7/Code/task3.c
@@ -4,14 +4,7 @@
 #include <signal.h>
 #include <sys/types.h>
 
-/*void child_process(){
-    int pid = getpid();
-    printf("Child Process PID: %d\n", pid);
-    sleep(5);
-}*/
-
 int main(void){
-    int i =1;
     pid_t pid;
     pid = fork();
 
@@ -23,47 +16,15 @@ int main(void){
         //Child Process
         int pid = getpid();
         printf("Child Process PID: %d\n", pid);
-        execl("/bin/sh", "./process", NULL);//hihiihihi
-        sleep(5);
+        char * argv_list[] = {"process",NULL}; 
+        execv(argv_list[0], argv_list);
         exit(0);
     }
     else {
         //Parent Process
-        wait(NULL);//Wait for Child Process to terminate
-        //sleep(1); //Wait for Child Process to Start
-        printf("Sending LeSignalJames to Child Process\n");
+        // wait 5 seconds then send interupt signal
+        sleep(5);
         kill(pid, SIGINT);
-        printf("LeChild has been Killed, Counter Terrorist Win\n");
         exit(0);
     }
 }
-
-
-/*
-int main(void)
-{
-    pid_t parent = getpid();
-    pid_t pid = fork();
-
-    if (pid == -1)
-    {
-        printf("Fork failed.");
-    } 
-    else if (pid > 0)
-    {
-        int status;
-        
-        printf("%d parentld\n",(int)pid);
-    }
-    else 
-    {
-        pid_t pid_process = getpid();
-        char * argv_list[] = {"process",NULL}; 
-        execv(argv_list[0], argv_list);
-        exit();
-        printf("%d child \n",(int)pid);
-        sleep(5);
-        
-        kill(pid, SIGINT);
-    }
-}*/
