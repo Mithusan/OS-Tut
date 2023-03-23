9ee0611c5174ad087482599954a304d327a76c06
Mithusan <mithusan.arul11@gmail.com>
2023-03-22 23:36:13 -0400

    Co-authored-by: 123here <123here@users.noreply.github.com> Co-authored-by: Mohammad Mohammadkhani <CaptainMetal080@users.noreply.github.com>

--- Tutorial 7/Code/task5.c
+++ Tutorial 7/Code/task5.c
@@ -1,136 +1,211 @@
+/**
+ * @file q5.c
+ * @author Group 12
+ * @brief 
+ * @version 1.0
+ * @date 2023-03-22
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+/**
+ * Import all necessary header files.
+*/
+
 #include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-#define MAX_NAME_LEN 256
-#define MAX_LINE_LEN 1024
+/**
+ * @brief Define constant to use throughout program. 
+ *        Ensures max length of a processes name is 
+ *        100 characters. Placing a leeway threshold 
+ *        based on the file provided --> processes_q5.txt.
+ * 
+ */
+#define MAX_NAME_LENGTH 100
 
-typedef struct proc{
-    char name[MAX_NAME_LEN];
+/**
+ * @brief Initiate a process struct data type with 
+ *        necessary fields name, priority, pid
+ *        and runtime. 
+ * 
+ *        Used to create instances of processes to 
+ *        queue and dequeue. 
+ * 
+ */
+struct process {
+    char name[MAX_NAME_LENGTH];
     int priority;
-    int pid;
+    pid_t pid;
     int runtime;
-}proc_t;
+    struct process *next;
+};
 
-typedef struct queue {
-    proc_t process;
-    struct queue* next;
-}queue_t;
-
-
-void push(queue_t** queue, proc_t process) {
-    queue_t* new_node = (queue_t*) malloc(sizeof(queue_t));
-    new_node->process = process;
-    new_node->next = *queue;
-    *queue = new_node;
+/**
+ * @brief Adds a new process to the linked list by creating a new struct process 
+ *        > setting its values based on the arguments passed to the function.
+ *        
+ *        If the linked list is empty > new process is added. 
+ *        
+ *        If the linked list isn't empty > add process to end of list.
+ *  
+ *        Return pointer to new list. 
+ * 
+ * @param head 
+ * @param new_process 
+ */
+void addProcess(struct process **head, struct process *new_process) {
+    if (*head == NULL) {
+        *head = new_process;
+    } else {
+        struct process *current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = new_process;
+    }
 }
 
-proc_t pop(queue_t** queue) {
-    queue_t* curr_node = *queue;
-    proc_t process = curr_node->process;
-    *queue = curr_node->next;
-    free(curr_node);
-    return process;
-}
 
-proc_t* delete_name(queue_t** queue, char* name) {
-    queue_t* curr_node = *queue;
-    queue_t* prev_node = NULL;
-    while (curr_node != NULL) {
-        if (strcmp(curr_node->process.name, name) == 0) {
-            if (prev_node == NULL) {
-                *queue = curr_node->next;
-            } 
-            else {
-                prev_node->next = curr_node->next;
+/**
+ * @brief Removes a process with the given name from the linked list.
+ *        
+ *        Searches for the node containing the process with the given name 
+ *        when found > removes it from the list.
+ *          
+ *        Returns a pointer to the linked list. 
+ * 
+ * @param head 
+ * @param name 
+ * @return struct process* 
+ */
+struct process *delete_name(struct process **head, char *name) {
+    struct process *current = *head;
+    struct process *previous = NULL;
+    while (current != NULL) {
+        if (strcmp(current->name, name) == 0) {
+            if (previous == NULL) {
+                *head = current->next;
+            } else {
+                previous->next = current->next;
             }
-        return &(curr_node->process);
+            return current;
+        }
+        previous = current;
+        current = current->next;
     }
-    prev_node = curr_node;
-    curr_node = curr_node->next;
-  }
-  return NULL;
+    return NULL;
 }
 
-proc_t* delete_pid(queue_t** queue, int pid) {
-    queue_t* curr_node = *queue;
-    queue_t* prev_node = NULL;
-    while (curr_node != NULL) {
-        if (curr_node->process.pid == pid) {
-            if (prev_node == NULL) {
-                *queue = curr_node->next;
-            } 
-            else {
-                prev_node->next = curr_node->next;
-            }
-        return &(curr_node->process);
+/**
+ * @brief Function to execute process by assigning process id to fork.
+ *        
+ *        Creates child process > execute process binary. 
+ *        
+ *        Have an error handler to ensure process is created. 
+ *        
+ *        Child process runs till specified runtime and parent
+ *        process waits for full completion.
+ * 
+ *        Parent process id is set to that of child using pointer.     
+ * 
+ *        Once child is fully terminated and parent receives exit
+ *        status --> print name, priority, pid, and runtime of the process.
+ *    
+ * @param p 
+ */
+void execProcess(struct process *p) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        char path[MAX_NAME_LENGTH + 2];
+        snprintf(path, MAX_NAME_LENGTH + 2, "./%s", p->name);
+        char *args[] = {path, NULL};
+        execvp(args[0], args);
+        exit(1);
+    } else if (pid < 0) {
+        printf("Error: Failed to fork process\n");
+        exit(1);
+    } else {
+        p->pid = pid;
+        sleep(p->runtime);
+        kill(p->pid, SIGINT);
+        waitpid(pid, NULL, 0);
+        printf("Name: %s, Priority: %d, PID: %d, Runtime: %d\n", p->name, p->priority, p->pid, p->runtime);
     }
-    prev_node = curr_node;
-    curr_node = curr_node->next;
-  }
-  return NULL;
 }
 
-int main(void){
-    //Variables
-    char line[MAX_LINE_LEN];
-    char* name_str;
-    char* priority_str;
-    char* pid_str;
-    char* runtime_str;
-    proc_t process;
-    queue_t* queue = NULL;
-    
-    // Open the file containing the processes
-    FILE *fp = fopen("processes_q5.txt", "r");
-
-    if (fp == NULL) {
-        perror("Error opening file");
-        return EXIT_FAILURE;
+/**
+ * @brief Main driver initializes a queue > read processes from file 
+ *        > processes_q5.txt > add them to queue > close file. 
+ *        
+ *        Iterate through queue and execute processes as long as
+ *        queue isn't empty.
+ * 
+ *        Based on the assignment requirements, execute processes
+ *        with a priority of zero first. 
+ * 
+ *        Once priority of zero process completed > remove from queue
+ *        using delete_name() function as advised. 
+ *         
+ *        For any process with a pid > 0, dequeue from front of the 
+ *        queue and execute the process after all priority processes
+ *        have completed. 
+ * 
+ *        Upon doing so will essentially print name, priority, pid, and 
+ *        runtime of the process by calling the execProcess() function. 
+ * 
+ *         Free up memory and allocation space by destroying the queue.
+ *        
+ * @return int 
+ */
+int main() {
+    FILE *file = fopen("processes_q5.txt", "r");
+    if (file == NULL) {
+        printf("Error: Failed to open processes_q5.txt\n");
+        exit(1);
     }
 
-    // Read the file line by line
-    while (fgets(line, MAX_LINE_LEN, fp) != NULL) {
-        // extract fields from line
-        name_str = strtok(line, ",");
-        priority_str = strtok(NULL, ",");
-        runtime_str = strtok(NULL, ",");
+    struct process *head = NULL;
 
-        // parse values and create process struct
-        strncpy(process.name, name_str, MAX_NAME_LEN);
-        process.priority = atoi(priority_str);
-        process.pid = 0;
-        process.runtime = atoi(runtime_str);
-
-        // add process to queue
-        push(&queue, process);
+    char name[MAX_NAME_LENGTH];
+    int priority, runtime;
+    while (fscanf(file, "%s %d %d", name, &priority, &runtime) != EOF) {
+        struct process *p = malloc(sizeof(struct process));
+        strcpy(p->name, name);
+        p->priority = priority;
+        p->pid = 0;
+        p->runtime = runtime;
+        p->next = NULL;
+        addProcess(&head, p);
     }
 
-    // Close the file
-    fclose(fp);
+    fclose(file);
 
-    // delete process named emacs
-    proc_t* deleted_process = delete_name(&queue, "emacs");
-    if (deleted_process != NULL) {
-        printf("Deleted process: %s\n", deleted_process->name);
+    struct process *current = head;
+    while (current != NULL) {
+        if (current->priority == 0) {
+            execProcess(current);
+            struct process *removed = delete_name(&head, current->name);
+            free(removed);
+            current = head;
+        } else {
+            current = current->next;
+        }
     }
 
-    // delete process with pid 12235
-    deleted_process = delete_pid(&queue, 12235);
-    if (deleted_process != NULL) {
-        printf("Deleted process: %d\n", deleted_process->pid);
-    }
-
-    printf("\n\n");
-
-    queue_t* curr_node = queue;
-    while (curr_node != NULL) {
-        printf("Name: %s\nPriority: %d\nPID: %d\nRuntime: %d", 
-            curr_node->process.name, curr_node->process.priority,
-            curr_node->process.pid, curr_node->process.runtime);
-        curr_node = curr_node->next;
-
-        printf("\n************************\n");
+    current = head;
+    while (current != NULL) {
+        execProcess(current);
+        struct process *removed = delete_name(&head, current->name);
+        free(removed);
+        current = head;
     }
 
     return 0;
