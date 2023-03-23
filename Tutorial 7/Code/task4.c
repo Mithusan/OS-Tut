e5510adf7c4aec19f751c66894bcf2cc5cc46b26
Mithusan <mithusan.arul11@gmail.com>
2023-03-22 22:28:26 -0400

    dsadad

--- Tutorial 7/Code/task4.c
+++ Tutorial 7/Code/task4.c
@@ -10,24 +10,26 @@ int main(void)
 	pid_t pid = fork();
 	
 		if(pid == -1){
-		printf("fork failed");
+			printf("fork failed");
 		}
 		
-		else if(pid > 0){
-		int status;
-		waitpid(pid, &status, 0);
+		else if(pid == 0){
+			pid_t pid_process = getpid();
+			
+			char * argv_list[] = {"process",NULL};
+			execv("process", argv_list);
+			exit(0);
 		}
 		
 		else{
-		pid_t pid_process = getpid();
-		int status;
-		char * argv_list[] = {"process",NULL};
-		execv("process", argv_list);
-		wait(5);
-		kill(pid_process,SIGTSTP);
-		wait(10);
-		kill(pid_process,SIGCONT);
-		exit(0);
+			wait(5);
+			kill(pid_process,SIGTSTP);
+			wait(10);
+			kill(pid_process,SIGCONT);
+			
+			int status;
+			waitpid(pid, &status, 0);
+			exit(0);
 		}
 	
 }
