dbeec954d93b226f721d896aa5e867af4972d31a
Mithusan <mithusan.arul11@gmail.com>
2023-03-28 15:39:10 -0400

    dsada

---
+++ Tutorial 8/Code/task1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+struct proc{
+    char parent[256];
+    char name[256];
+    int priority;
+    int memory;//in MB
+};
+
+int main(){
+    
+}
